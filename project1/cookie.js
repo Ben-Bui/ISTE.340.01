514d062b8447de6301049b7ce713371a3634d116
Ben B <btb4516@g.rit.edu>
2025-02-18 10:41:14 -0500

    change cookies

--- project1/cookie.js
+++ project1/cookie.js
@@ -1,20 +1,49 @@
-function setCookie(name, value, days) {
-    let expires = "";
-    if (days) {
-        let date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
+// cookies.js
+// You can use this code for your projects!
+// Derived from the Bill Dortch code at http://www.hidaho.com/cookies/cookie.txt
 
-function getCookie(name) {
-    let nameEQ = name + "=";
-    let ca = document.cookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
+function getCookieVal (offset) {
+	var endstr = document.cookie.indexOf (";", offset);
+	if (endstr == -1) { endstr = document.cookie.length; }
+	return decodeURIComponent(document.cookie.substring(offset, endstr));
+	}
+
+function GetCookie (name) {
+	var arg = name + "=";
+	var alen = arg.length;
+	var clen = document.cookie.length;
+	var i = 0;
+	while (i < clen) {
+		var j = i + alen;
+		if (document.cookie.substring(i, j) == arg) {
+			return getCookieVal (j);
+			}
+		i = document.cookie.indexOf(" ", i) + 1;
+		if (i == 0) break; 
+		}
+	return null;
+	}
+
+function DeleteCookie (name,path,domain) {
+	if (GetCookie(name)) {
+		document.cookie = name + "=" +
+		((path) ? "; path=" + path : "") +
+		((domain) ? "; domain=" + domain : "") +
+		"; expires=Thu, 01-Jan-70 00:00:01 GMT";
+		}
+	}
+
+/////////
+// use:
+//		SetCookie('name', 'value', 3000);
+//		SetCookie('name', 'value', 1000,false,false,false,true);
+//		If set the secure (last arg) to true, you MUST be on an https connection!
+/////////
+function SetCookie (name,value,maxAge,path,domain,sameSite,secure) {
+  document.cookie = name + "=" + encodeURIComponent(value) +
+    ((maxAge) ? ";max-age=" + maxAge  : "") +
+    ((path) ? ";path=" + path  : "") +
+    ((domain) ? ";domain=" + domain : "") +
+    ((sameSite) ? ";samesite=" + sameSite : ";samesite=strict") +
+    ((secure) ? ";secure;" : ";");
 }
