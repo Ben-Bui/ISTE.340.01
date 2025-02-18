f9e3a38d962377503ab4e581b4cc2200dcfd0647
Ben B <btb4516@g.rit.edu>
2025-02-18 10:46:01 -0500

    more option

--- project1/data.js
+++ project1/data.js
@@ -1,15 +1,29 @@
-var characterData = {
-    init: ["Choose your Race:", "Human", "Elf", "Dwarf"],
-    Human: ["Choose your Class:", "Warrior", "Mage"],
-    Elf: ["Choose your Class:", "Ranger", "Sorcerer"],
-    Dwarf: ["Choose your Class:", "Paladin", "Berserker"],
+var data = {
+    init: ["Choose your race:", "Human", "Elf", "Dragonian"],
+    Human: ["Choose your class:", "Fighter", "Wizard", "Rogue"],
+    Elf: ["Choose your class:", "Ranger", "Sorcerer", "Druid"],
+    Dragonian: ["Choose your class:", "Barbarian", "Cleric", "Paladin"],
     
-    Warrior: ["Choose your Background:", "Soldier", "Mercenary"],
+    Fighter: ["Choose your weapon:", "Sword", "Axe"],
+    Wizard: ["Choose your spell:", "Fireball", "Lightning"],
+    Rogue: ["Choose your skill:", "Stealth", "Lockpicking"],
+
+    Ranger: ["Choose your weapon:", "Bow", "Dagger"],
+    Sorcerer: ["Choose your spell:", "Ice Spear", "Dark Wave"],
+    Druid: ["Choose your ability:", "Shape-shift", "Heal"],
+
+    Barbarian: ["Choose your rage:", "Frenzy", "Endurance"],
+    Cleric: ["Choose your divine power:", "Holy Smite", "Blessing"],
+    Paladin: ["Choose your oath:", "Oath of Vengeance", "Oath of Protection"]
+};
+var characterData = {
+    
+    Fighter: ["Choose your Background:", "Soldier", "Mercenary"],
     Mage: ["Choose your Background:", "Scholar", "Hermit"],
     Ranger: ["Choose your Background:", "Scout", "Hunter"],
     Sorcerer: ["Choose your Background:", "Acolyte", "Wanderer"],
     Paladin: ["Choose your Background:", "Knight", "Crusader"],
-    Berserker: ["Choose your Background:", "Raider", "Gladiator"],
+    Barbarian: ["Choose your Background:", "Raider", "Gladiator"],
 
     Soldier: ["Choose your Weapon:", "Sword", "Axe"],
     Mercenary: ["Choose your Weapon:", "Dagger", "Bow"],
