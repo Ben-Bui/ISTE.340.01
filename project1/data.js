77af40c287e680dc09addcf91c9aff2e0061c547
Ben B <btb4516@g.rit.edu>
2025-02-18 10:27:10 -0500

    project1

---
+++ project1/data.js
@@ -0,0 +1,26 @@
+var characterData = {
+    init: ["Choose your Race:", "Human", "Elf", "Dwarf"],
+    Human: ["Choose your Class:", "Warrior", "Mage"],
+    Elf: ["Choose your Class:", "Ranger", "Sorcerer"],
+    Dwarf: ["Choose your Class:", "Paladin", "Berserker"],
+    
+    Warrior: ["Choose your Background:", "Soldier", "Mercenary"],
+    Mage: ["Choose your Background:", "Scholar", "Hermit"],
+    Ranger: ["Choose your Background:", "Scout", "Hunter"],
+    Sorcerer: ["Choose your Background:", "Acolyte", "Wanderer"],
+    Paladin: ["Choose your Background:", "Knight", "Crusader"],
+    Berserker: ["Choose your Background:", "Raider", "Gladiator"],
+
+    Soldier: ["Choose your Weapon:", "Sword", "Axe"],
+    Mercenary: ["Choose your Weapon:", "Dagger", "Bow"],
+    Scholar: ["Choose your Weapon:", "Staff", "Orb"],
+    Hermit: ["Choose your Weapon:", "Wand", "Tome"],
+    Scout: ["Choose your Weapon:", "Short Bow", "Crossbow"],
+    Hunter: ["Choose your Weapon:", "Long Bow", "Spear"],
+    Acolyte: ["Choose your Weapon:", "Magic Staff", "Holy Symbol"],
+    Wanderer: ["Choose your Weapon:", "Dagger", "Slingshot"],
+    Knight: ["Choose your Weapon:", "Greatsword", "Lance"],
+    Crusader: ["Choose your Weapon:", "Mace", "Warhammer"],
+    Raider: ["Choose your Weapon:", "Battle Axe", "Flail"],
+    Gladiator: ["Choose your Weapon:", "Dual Daggers", "Spiked Club"]
+};
