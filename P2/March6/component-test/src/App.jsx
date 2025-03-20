f2c728b380314adcb94aa79a85d4c5009850d3c4
Ben Bui <benthebui@gmail.com>
2025-03-20 09:33:30 -0400

    class march 18

--- P2/March6/component-test/src/App.jsx
+++ P2/March6/component-test/src/App.jsx
@@ -1,7 +1,9 @@
 //import hooks
+import {useState} from 'react';
 
 //import in our component
 import Welcome from './components/funcComp.jsx'
+import Box from './components/box.jsx';
 
 //bring in our CSS
 import './App.css'
@@ -12,15 +14,49 @@ function WelcomeInternal({ name }) {
 }
 
 function App() {
-  return (
+  //build in my state!
+  //const [getter, setter] = useState(init);
+  //loaded is a var that flips the switch when I get data!
+  const[loaded, setLoaded] = useState(false);
+  //myObj is a var that holds my returned data!
+  const[myObj, setMyObj] = useState();
+
+  //define all my internal functions!
+  const getData = () =>{
+    console.log('in the middle of the function');
+    setMyObj(
+      {
+        title:"React is so much fun!",
+        description:"lots and lots of words, something meaningful, yada, yada ...",
+        age:21,
+        address:1234
+       
+      }
+    );
+    setLoaded(true);
+  }
+
+  //can i do a console.log
+  console.log('in the middle of app');
+
+  if (!loaded) return (
+    //console.log('fd') No!
     <>
+    {console.log('test inside of return!')}
       <h1 >
         Ben's First React Project!
       </h1>
-      <WelcomeInternal name="Ben" />
-      <Welcome name="Dean" job="talking" someStyle="otherStyle" />
-      <Welcome name="Sam" job="boss" someStyle="myStyle" />
-      <p>some test</p>
+
+      <button onClick={getData}>Get the data!</button>
+    </>
+  )
+
+  return(
+    <>
+      <h1>we now have data</h1>
+      <h3>{myObj.title}</h3>
+      <h3>{myObj.description}</h3>
+      <Box age={myObj.age} address={{myObj}}/>
     </>
   )
 }
---
+++ P2/March6/component-test/src/components/box.jsx
@@ -0,0 +1,11 @@
+const box = ({address, age})=>{
+    return(
+    <div>
+        <h1>Hello!</h1>
+        <h5>you live at: {address}</h5>
+        <h5>you are old! {age}</h5>
+    </div>
+    )
+}
+
+export default box
