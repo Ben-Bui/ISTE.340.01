fef830dc92a4c37e0c62f6f7cc2ac5786f08d7f4
Ben Bui <benthebui@gmail.com>
2025-03-27 10:42:23 -0400

    march 27

--- P2/March20/project2-ben-1/src/components/PeopleGroup.jsx
+++ P2/March20/project2-ben-1/src/components/PeopleGroup.jsx
@@ -1,7 +1,23 @@
 //import 
+import PeopleModal from "./PeopleModal.jsx";
 
-const PeopleGroup=()=>{
+const PeopleGroup=({title, pepGroup})=>{
 
+    return(
+        <>
+            <h1>{title}</h1>
+            <div className="peopleList" >
+                {pepGroup.map( (p)=>
+                    <div className="peopleListItem">
+                        <img alt="person" src={p.imagePath}></img>
+                        <h3>{p.name}</h3>
+                        <PeopleModal {...p}/>
+                    </div>
+                )}
+
+            </div>
+        </>
+    );
 }
 
 export default PeopleGroup;
--- P2/March20/project2-ben-1/src/components/PeopleTabs.jsx
+++ P2/March20/project2-ben-1/src/components/PeopleTabs.jsx
@@ -1,23 +1,46 @@
+import { useState, useEffect } from 'react';
+
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 
 import PeopleGroup from './PeopleGroup';
 
+import getData from '../util/GetData';
+import './People.css'
+
 function PeopleTabs() {
+  //state
+  const [loaded, setLoaded] = useState(false);
+  const [people, setPeople] = useState();
+
+  useEffect( ()=>{
+    getData('people/')
+      .then((json)=>{
+        setPeople(json);
+        setLoaded(true);
+      });
+  }, [] );
+
+  if(!loaded) return(<h1>...Loading People...</h1>)
   return (
+    <>
+    <h1>{people.title}</h1>
+    <h3>{people.subtitle}</h3>
+    
     <Tabs
       defaultActiveKey="fac"
       id="uncontrolled-tab-example"
       className="mb-3"
     >
       <Tab eventKey="fac" title="Faculty">
-        <PeopleGroup/ >
+        <PeopleGroup title={"Faculty"} pepGroup={people.faculty}/>
       </Tab>
       <Tab eventKey="profile" title="Staff">
-        <PeopleGroup/>
+        <PeopleGroup title={"Staff"} pepGroup={people.staff}/>
       </Tab>
 
     </Tabs>
+    </>
   );
 }
 
